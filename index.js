a8b375f2087399b9fd289420ce1bd541a8f3fb05
shinaz <shinattey10@gmail.com>
2021-09-22 16:51:03 +0500

    Named and default exports

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { Teacher } from "./teacher";
+import Teacher, {promote} from "./teacher";
+import React, { component } from 'react';
+
+// Default - import ... from '';
+// Named -> import { ... } from '';
 
 const teacher = new Teacher("John", "MSc");
 teacher.teach();
